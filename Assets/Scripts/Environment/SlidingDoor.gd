70b92d4fd8671323b936d32676ef10c33f44819e
Timur <huntertring87@gmail.com>
2026-07-02 21:15:32 +0300

    fix door

--- Assets/Scripts/Environment/SlidingDoor.gd
+++ Assets/Scripts/Environment/SlidingDoor.gd
@@ -6,6 +6,13 @@ class_name SlidingDoor
 
 var is_open : bool = false
 var slide_tween : Tween
+var start_position : Vector3
+var end_position : Vector3
+
+func _ready() -> void:
+	start_position = self.global_position
+	end_position = start_position + target_offset
+
 
 func interact():
 	is_open = !is_open
@@ -13,12 +20,19 @@ func interact():
 	
 
 func change_door_position(is_open : bool):
-	var side = 1 if is_open == true else -1
-	
-	if slide_tween:
-		slide_tween.kill()
-		
-	slide_tween = create_tween()
-	slide_tween.set_ease(Tween.EASE_IN_OUT)
-	slide_tween.set_trans(Tween.TRANS_LINEAR)
-	slide_tween.tween_property(self, "position", position + target_offset * side, 0.5)
+	if is_open:
+		if slide_tween:
+			slide_tween.kill()
+			
+		slide_tween = create_tween()
+		slide_tween.set_ease(Tween.EASE_IN_OUT)
+		slide_tween.set_trans(Tween.TRANS_LINEAR)
+		slide_tween.tween_property(self, "position", start_position, 0.5)
+	else:
+		if slide_tween:
+			slide_tween.kill()
+			
+		slide_tween = create_tween()
+		slide_tween.set_ease(Tween.EASE_IN_OUT)
+		slide_tween.set_trans(Tween.TRANS_LINEAR)
+		slide_tween.tween_property(self, "position", end_position, 0.5)
