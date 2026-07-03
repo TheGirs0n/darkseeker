373adcb7400ee1ebf15486bdac06ec4a2a468d86
Timur <huntertring87@gmail.com>
2026-07-03 21:44:04 +0300

    fix 2 terminal

--- Assets/Scripts/Environment/MiniGamesTerminal.gd
+++ Assets/Scripts/Environment/MiniGamesTerminal.gd
@@ -4,6 +4,7 @@ class_name MiniGamesTerminal
 @export var mini_game_panel : PackedScene
 
 var active_panel : MiniGamesPanel = null
+var active_interactor : CharacterBody3D = null
 
 func interact(interactor : CharacterBody3D):
 	if active_panel == null:
@@ -12,11 +13,15 @@ func interact(interactor : CharacterBody3D):
 		active_panel.finished.connect(mini_game_done)
 		get_tree().root.add_child(scene)
 		
-		interactor.set_controls_disable()
+		active_interactor = interactor
+		active_interactor.set_controls_disable()
 		
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 
 func mini_game_done(_done : bool):
 	active_panel = null
+	active_interactor.set_controls_enable()
+	active_interactor = null
+	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
