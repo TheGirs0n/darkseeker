0d7a01a57daa5a737a98880fca951334d16ebd7a
Timur <huntertring87@gmail.com>
2026-07-05 22:19:25 +0300

    line

--- Assets/Scripts/Environment/MiniGames/MiniGamesScene/Wire/MiniGameWire.gd
+++ Assets/Scripts/Environment/MiniGames/MiniGamesScene/Wire/MiniGameWire.gd
@@ -12,12 +12,14 @@ var all_wires : Array[WireSlot]
 
 func _setup(interactor : CharacterBody3D, mini_game_config : MiniGamesConfig):
 	var wire_game_config : MiniGamesWireConfig = mini_game_config
+	var first_color_array = wire_slots_color_array
+	var second_color_array = first_color_array
 	
 	wire_slots_color_array.shuffle()
 	
 	for i in wire_game_config.wire_pairs:
 		var wire_slot = wire_slot_scene.instantiate()
-		var color_random = wire_slots_color_array.pick_random()
+		var color_random = first_color_array.pop_front()
 		
 		wire_slot.setup_wire_slot(color_random)
 		wire_slot.wire_slot_click.connect(wire_check)
@@ -28,7 +30,7 @@ func _setup(interactor : CharacterBody3D, mini_game_config : MiniGamesConfig):
 	
 	for i in wire_game_config.wire_pairs:
 		var wire_slot = wire_slot_scene.instantiate()
-		var color_random = wire_slots_color_array.pick_random()
+		var color_random = second_color_array.pop_front()
 		
 		wire_slot.setup_wire_slot(color_random)
 		wire_slot.wire_slot_click.connect(wire_check)
@@ -37,12 +39,19 @@ func _setup(interactor : CharacterBody3D, mini_game_config : MiniGamesConfig):
 		
 
 func wire_check(wire_slot : WireSlot):
+	if selected_wire_slot == wire_slot:
+		return
+	
 	if selected_wire_slot == null:
 		selected_wire_slot = wire_slot
 	else:
 		if selected_wire_slot.modulate == wire_slot.modulate:
 			selected_wire_slot.connect_wire()
 			wire_slot.connect_wire()
+			
+			selected_wire_slot = null
+			
+			_win()
 		else:
 			selected_wire_slot.button_pressed = false
 			wire_slot.button_pressed = false
@@ -55,3 +64,13 @@ func _win():
 			return
 			
 	super._win()
+
+
+func set_line(dot_first : WireSlot, dot_second : WireSlot):
+	var line = Line2D.new()
+	line.width = 6
+	line.default_color = dot_first.modulate
+	var top_center = dot_first.global_position + dot_first.size / 2
+	var bottom_center = dot_second.global_position + dot_second.size / 2
+	line.points = [line.to_local(top_center), line.to_local(bottom_center)]
+	add_child(line)
