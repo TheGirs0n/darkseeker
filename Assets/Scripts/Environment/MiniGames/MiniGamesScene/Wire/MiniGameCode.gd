6af60ea95a66f5cf1c0cbdcc468bea57d2e298e5
Timur <huntertring87@gmail.com>
2026-07-08 22:04:05 +0300

    cod1

--- Assets/Scripts/Environment/MiniGames/MiniGamesScene/Wire/MiniGameCode.gd
+++ Assets/Scripts/Environment/MiniGames/MiniGamesScene/Wire/MiniGameCode.gd
@@ -6,17 +6,34 @@ class_name MiniGameCode
 @export var second_slot : Label
 @export var third_slot : Label
 
+var code_alphabet : String
+var code_letters : String
+var code_current_index : int = 0
 
 func _input(event: InputEvent) -> void:
-	pass
-
-
+	if event is InputEventKey and event.is_pressed():
+		var symbol = char(event.unicode)
+		if symbol in code_alphabet:
+			if code_letters[code_current_index] == symbol:
+				code_current_index += 1
+				_win()
+			else:
+				code_current_index = 0
+				
+				
 func _setup(interactor : CharacterBody3D, mini_game_config : MiniGamesConfig):
 	var code_config : MiniGamesCodeConfig = mini_game_config 
 	set_numbers(code_config)
 
 
 func set_numbers(code_config : MiniGamesCodeConfig):
-	first_slot.text = code_config.mini_game_code_alphabet[randi_range(0, code_config.mini_game_code_alphabet.length() - 1)]
-	second_slot.text = code_config.mini_game_code_alphabet[randi_range(0, code_config.mini_game_code_alphabet.length() - 1)]
-	third_slot.text = code_config.mini_game_code_alphabet[randi_range(0, code_config.mini_game_code_alphabet.length() - 1)]
+	code_alphabet = code_config.mini_game_code_alphabet
+	
+	#code_letters = code_config.mini_game_code_alphabet.
+
+
+func _win():
+	if code_current_index != 2:
+		return
+	else:
+		super._win()
