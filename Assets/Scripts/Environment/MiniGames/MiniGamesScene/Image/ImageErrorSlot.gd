23ee0295a7266849f75e796754ac162a3875e131
Timur <huntertring87@gmail.com>
2026-07-12 20:38:53 +0300

    IMAGE FIX

--- Assets/Scripts/Environment/MiniGames/MiniGamesScene/Image/ImageErrorSlot.gd
+++ Assets/Scripts/Environment/MiniGames/MiniGamesScene/Image/ImageErrorSlot.gd
@@ -4,6 +4,7 @@ class_name ImageErrorSlot
 
 signal error_image_pressed(error_image : ImageErrorSlot)
 
+
 func set_normal_texture(texture : CompressedTexture2D):
 	texture_normal = texture
 
--- Assets/Scripts/Environment/MiniGames/MiniGamesScene/Image/MiniGameImage.gd
+++ Assets/Scripts/Environment/MiniGames/MiniGamesScene/Image/MiniGameImage.gd
@@ -8,50 +8,52 @@ class_name MiniGameImage
 @export_group("Containers Center Node")
 @export var first_contaier_center_node_container : HBoxContainer
 
-
-var first_right_image : ImageErrorSlot = null
-var second_right_image : ImageErrorSlot = null
+var references : Array[CompressedTexture2D]
 
 var error_count : int
 var right_count : int
 
-func _setup(_interactor : CharacterBody3D, mini_game_config : MiniGamesConfig):
-	_setup_errors(mini_game_config)
-	
+var found_count : int = 0
 
-func _setup_errors(mini_game_config : MiniGamesImageConfig):
+func _setup(_interactor : CharacterBody3D, mini_game_config : MiniGamesConfig):
 	error_count = mini_game_config.mini_game_image_errors
 	right_count = mini_game_config.mini_game_image_count
 	
-	var images_array = mini_game_config.mini_game_image_array.duplicate().slice(0, error_count - 1)
-	images_array.shuffle()
+	_setup_errors(mini_game_config)
 	
-	_setup_container(first_container, error_count, images_array)
+	
+func _setup_errors(mini_game_config : MiniGamesImageConfig):
+	var image_array = mini_game_config.mini_game_image_array
 
+	var images_error = image_array.duplicate()
+	images_error.shuffle()
+	
+	var images_error_slice = images_error.slice(0, error_count)
+	
+	for error in range(right_count):
+		references.append(images_error_slice[error])
+		var texture = TextureRect.new()
+		texture.texture = images_error_slice[error]
+		texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		
+		first_contaier_center_node_container.add_child(texture)
+	
+	images_error_slice.shuffle()
+	
+	for error in range(error_count):
+		var error_slot = image_error_slot.instantiate()
+		error_slot.set_normal_texture(images_error_slice[error])
+		error_slot.error_image_pressed.connect(check_error_image)
+		
+		first_container.add_child(error_slot)
+		
 
-func _setup_container(container : HBoxContainer, error_count : int, images_array : Array[CompressedTexture2D]):
-	for i in range(error_count):
-		var error_image = image_error_slot.instantiate() as ImageErrorSlot
-		error_image.set_normal_texture(images_array.pick_random())
-		error_image.error_image_pressed.connect(check_error_image)
+func check_error_image(error_slot : ImageErrorSlot):
+	if error_slot.texture_normal in references and found_count != right_count:
+		error_slot.disabled = true
+		found_count += 1
 		
-		container.add_child(error_image)
-		
-	for i in range(error_count):
-		var error_image = image_error_slot.instantiate() as ImageErrorSlot
-		error_image.set_normal_texture(images_array.pick_random())
-		
-		container.add_child(error_image)
-		
-
-func check_error_image(error_image : ImageErrorSlot):
-	if error_image.texture_normal == first_right_image or error_image.texture_normal == second_right_image:
-		if error_image.texture_normal == first_right_image:
-			error_image.button_pressed = true
-		if error_image.texture_normal == second_right_image:
-			error_image.button_pressed = true
-			
-		if first_right_image.button_pressed == true and second_right_image.button_pressed == true:
+		if found_count == right_count:
 			_win()
 	else:
 		apply_time_penalty(1)
