5d9f4f4494122b912ba735ba67c87f8fad86026b
Timur <huntertring87@gmail.com>
2026-07-12 16:24:25 +0300

    add timer to mingame

--- Assets/Scripts/Environment/MiniGames/MiniGamesBase.gd
+++ Assets/Scripts/Environment/MiniGames/MiniGamesBase.gd
@@ -4,6 +4,8 @@ class_name MiniGamesBase
 
 var _active : bool = false
 var mini_game_difficulty : GlobalEnums.BotDifficulty = GlobalEnums.BotDifficulty.EASY
+var mini_game_timer_waittime : float
+var mini_game_timer_active : bool = false
 
 signal finished(state : bool)
 
@@ -12,10 +14,29 @@ func start_mini_game(interactor : CharacterBody3D, mini_game_config : MiniGamesC
 	_active = true
 	_setup(interactor, mini_game_config)
 	
+	if mini_game_config.mini_game_fail_time > 0:
+		mini_game_timer_waittime = mini_game_config.mini_game_fail_time
+		mini_game_timer_active = true
+		
+	
 
 func _setup(interactor : CharacterBody3D, mini_game_config : MiniGamesConfig):
 	pass
 	
+
+func _process(delta: float) -> void:
+	if not _active or not mini_game_timer_active:
+		return
+		
+	mini_game_timer_waittime -= delta
+	
+	if mini_game_timer_waittime <= 0:
+		_lose()
+
+
+func apply_time_penalty(seconds : float):
+	mini_game_timer_waittime = seconds
+
 
 func _win():
 	_finish(true)
