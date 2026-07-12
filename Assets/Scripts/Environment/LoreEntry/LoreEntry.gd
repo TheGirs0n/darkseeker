c358dba6533a281ffe812e7ed18ae364e8a50962
Timur <huntertring87@gmail.com>
2026-07-12 21:11:02 +0300

    part lore

---
+++ Assets/Scripts/Environment/LoreEntry/LoreEntry.gd
@@ -0,0 +1,10 @@
+extends StaticBody3D
+class_name LoreEntry
+
+
+var lore_entry_res : LoreEntryResource
+var active_interactor : CharacterBody3D
+
+
+func interact(_interactor : CharacterBody3D):
+	pass
---
+++ Assets/Scripts/Environment/LoreEntry/LoreEntryConfig.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name LoreEntryResource
+
+
+@export_group("Lore Entry Config")
+@export var lore_entry_id : int = 0
+@export var lore_entry_title : String
+@export_multiline var lore_entry_text : String
