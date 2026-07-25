68fb740d27413ea2c87e1fdb11101a8170e28da3
Timur <huntertring87@gmail.com>
2026-07-25 19:21:03 +0300

    123цуыйц

--- Assets/Scripts/Enemy/EnemyStateMachine/ChaseState.gd
+++ Assets/Scripts/Enemy/EnemyStateMachine/ChaseState.gd
@@ -6,8 +6,9 @@ class_name ChaseState
 
 func update_state(delta : float):
 	print("CHASE")
-	if current_state_machine.parent_node.enemy_detect_component.last_seen_posiion != Vector3.ZERO:
-		current_state_machine.parent_node.enemy_move_component.move_to(current_state_machine.parent_node.enemy_detect_component.last_seen_posiion)
+	var enemy = current_state_machine.parent_node
+	if enemy.enemy_detect_component.last_seen_posiion != Vector3.ZERO:
+		enemy.enemy_move_component.move_to(enemy.enemy_detect_component.last_seen_posiion)
 	
-	if !current_state_machine.parent_node.enemy_detect_component.check_player_detect():
+	if !enemy.enemy_detect_component.check_player_detect():
 		current_state_machine.change_state(patrol_state)
