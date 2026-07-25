6ab69fe09b3d563b05a55e9586a3860b6711ee4b
Timur <huntertring87@gmail.com>
2026-07-25 22:44:21 +0300

    asds

---
+++ Assets/Scripts/Environment/Noiser.gd
@@ -0,0 +1,24 @@
+extends StaticBody3D
+class_name Noiser
+
+@export var noise_area : Area3D
+@export var lifetime_timer : Timer
+
+var is_active : bool = false
+
+
+func interact(interactor : CharacterBody3D):
+	is_active = true
+	lifetime_timer.start()
+	
+
+func _physics_process(delta: float) -> void:
+	if not is_active:
+		return
+		
+	for body in noise_area.get_overlapping_bodies():
+		body.enemy_detect_component.check_hear(global_position)
+		
+		
+func timer_timerout():
+	is_active = false
